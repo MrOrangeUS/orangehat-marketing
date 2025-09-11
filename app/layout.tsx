af00a6ada5fa6cb73aa94be17dde203a69614a19
MrOrangeUS <71477247+MrOrangeUS@users.noreply.github.com>
2025-09-11 12:28:16 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,15 @@
-import './styles/globals.css';export default function RootLayout({children}){return(<html lang='en'><body>{children}</body></html>)}
+import "./styles/globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Orange Hat Marketing",
+  description: "We make your marketing impossible to ignore.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
